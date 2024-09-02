f99f7367a3cae2578123fd635d8ab2229d6ff26f
Brendo Gomes <brensogs2021@gmail.com>
2024-09-02 09:39:45 -0300

    aula 73 - jogo do dia, card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
+import { BsArrowRightSquare } from 'react-icons/bs'
+
 import Container from "@/components/container";
 import { GameProps } from "@/utils/types/game";
- 
-async function getDalyGame(){
-  try{
-    const response = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`);
+
+async function getDalyGame() {
+  try {
+    const response = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 } });
 
     return response.json();
 
-  }catch(err){
-    throw new Error("failed to fetch data ")
+  } catch (err) {
+    console.log(err);
   }
 }
 
@@ -20,21 +22,31 @@ export default async function Home() {
 
   return (
     <main className="w-full">
-        <Container>
-          <h1 className="text-center font-bold text-xl mt-8 mb-5">Sepramos um jogo exclusico pra voce</h1>
-           <Link href={`/game/${dalyGame.id}`}>
-           <section className="w-full bg-black rounded-lg">
-            <Image
-              src={dalyGame.image_url}
-              alt={dalyGame.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
-           </section>
-           </Link>
-        </Container>
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">Sepramos um jogo exclusico pra voce</h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative">
+
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#fff"/>
+              </div>
+
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
     </main>
   );
 }
